39c92a8a55c293dc86b40cb80e071443d365cf67
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2020-10-21 15:22:55 -0300

    Added generate files command

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+import os
 import urllib.request
+
+actual_branch = os.popen('git rev-parse --abbrev-ref HEAD').read().replace('\n', '')
+
+os.system("git checkout gh-pages")
+os.system("git reset --hard " + actual_branch)
 
 pages = {
     'health': '/health',
@@ -16,3 +22,8 @@ for page in pages:
         base_url + pages[page],
         page + ".html"
     )
+
+os.system("git add -A")
+os.system("git commit -m 'Generated files'")
+os.system("git push")
+os.system("git checkout " + actual_branch)
